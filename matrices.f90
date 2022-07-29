5077c5925997a73290bb94dc27bb58c8bcb65a37
Yuting Huang <38852271+ythuang96@users.noreply.github.com>
2022-07-29 15:56:57 -0700

    enforce dp in matrices

--- matrices.f90
+++ matrices.f90
@@ -1,8 +1,9 @@
 module matrices
-
+    use types, only: sp, dp
+    implicit none
     private
     include 'parameters'
 
-    real(8),public :: dt11(5,my),dt12(5,my)
+    real(kind=dp),public :: dt11(5,my),dt12(5,my)
 
 end module matrices
